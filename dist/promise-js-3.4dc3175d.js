62c1dcfba2a5fc8c8e4bc312a16823e8f379df2d
AndNeeeeewSite <pavlopysarenko1234@gmail.com>
2025-04-09 23:39:28 +0300

    !

--- dist/promise-js-3.4dc3175d.js
+++ dist/promise-js-3.4dc3175d.js
@@ -677,7 +677,7 @@ input = document.querySelector('#search');
 input.addEventListener('input', _.debounce(function() {
     console.log(input.value);
     if (input.value == " " || input.value == undefined) PNotify.alert({
-        text: 'Notice me, senpai!'
+        text: 'Print name of country!'
     });
     else fetch('https://restcountries.com/v2/name/' + input.value).then((response)=>{
         if (!response.ok) throw new Error('Offline');
@@ -686,7 +686,68 @@ input.addEventListener('input', _.debounce(function() {
         if (data.length > 10) PNotify.error({
             text: 'Too many matches found. Please enter a more specific query!'
         });
-        else console.log(data);
+        else {
+            console.log(data);
+            document.querySelector('.main-out').remove();
+            div = document.createElement('div');
+            div.classList.add('main-out');
+            document.querySelector('main').append(div);
+            if (data.length === 1) {
+                console.log(data.length);
+                country = data[0];
+                //       Main Div
+                divMain = document.createElement('div');
+                divMain.classList.add('divMain');
+                //     Country name
+                item = document.createElement('h2');
+                item.textContent = country.name;
+                item.style.textAlign = 'center';
+                divMain.append(item);
+                //       Main Info Div
+                mainInfoDiv = document.createElement('div');
+                mainInfoDiv.classList.add('mainInfoDiv');
+                divMain.append(mainInfoDiv);
+                //     Info Div
+                infoDiv = document.createElement('div');
+                infoDiv.classList.add('infoDiv');
+                mainInfoDiv.append(infoDiv);
+                //        County capital
+                capital = document.createElement('h2');
+                capital.innerHTML = '<span>Capital: </span> ' + country.capital;
+                infoDiv.append(capital);
+                //        County population
+                population = document.createElement('h2');
+                population.innerHTML = '<span>Population: </span> ' + country.population;
+                infoDiv.append(population);
+                //        County language
+                languages = document.createElement('h2');
+                languages.innerHTML = '<span>Languages: </span> ';
+                infoDiv.append(languages);
+                langUl = document.createElement('ul');
+                languages = country.languages;
+                for (lang of languages){
+                    langName = lang.name;
+                    langLi = document.createElement('li');
+                    langTitle = document.createElement('h2');
+                    langTitle.textContent = langName;
+                    langLi.append(langTitle);
+                    langUl.append(langLi);
+                }
+                infoDiv.append(langUl);
+                console.log(languages);
+                //   Country flag
+                image = document.createElement('img');
+                image.src = country.flags.png;
+                mainInfoDiv.append(image);
+                console.log(divMain);
+                // Final add
+                document.querySelector('.main-out').append(divMain);
+            } else for (country of data){
+                item = document.createElement('h2');
+                item.textContent = country.name;
+                document.querySelector('.main-out').append(item);
+            }
+        }
     }).catch((error)=>{
         console.error('Error:', error);
     });
--- js/fetchCountries.js
+++ js/fetchCountries.js
@@ -9,7 +9,7 @@ input.addEventListener('input',_.debounce(function(){
     console.log(input.value)
     if(input.value == " " || input.value == undefined){
       PNotify.alert({
-        text: 'Notice me, senpai!'
+        text: 'Print name of country!'
       });
     }
     else{
@@ -25,9 +25,76 @@ input.addEventListener('input',_.debounce(function(){
           PNotify.error({
             text: 'Too many matches found. Please enter a more specific query!'
           });
+          
         }
         else{
           console.log(data)
+          
+          document.querySelector('.main-out').remove()
+          div = document.createElement('div')
+          div.classList.add('main-out')
+          document.querySelector('main').append(div)
+          if(data.length === 1){
+            console.log(data.length)
+            country = data[0]
+            //       Main Div
+            divMain = document.createElement('div')
+            divMain.classList.add('divMain')
+            //     Country name
+            item = document.createElement('h2')
+            item.textContent = country.name
+            item.style.textAlign = 'center'
+            divMain.append(item)
+            //       Main Info Div
+            mainInfoDiv = document.createElement('div')
+            mainInfoDiv.classList.add('mainInfoDiv')
+            divMain.append(mainInfoDiv  )
+            //     Info Div
+            infoDiv = document.createElement('div')
+            infoDiv.classList.add('infoDiv')
+            mainInfoDiv.append(infoDiv)
+
+            //        County capital
+            capital = document.createElement('h2')
+            capital.innerHTML = '<span>Capital: </span> ' + country.capital
+            infoDiv.append(capital)
+            //        County population
+            population = document.createElement('h2')
+            population.innerHTML = '<span>Population: </span> ' + country.population
+            infoDiv.append(population)
+            //        County language
+            languages = document.createElement('h2')
+            languages.innerHTML = '<span>Languages: </span> '
+            infoDiv.append(languages)
+            langUl = document.createElement('ul')
+            languages = country.languages
+            for(lang of languages){
+              langName = lang.name
+              langLi = document.createElement('li')
+              langTitle = document.createElement('h2')
+              langTitle.textContent = langName
+              langLi.append(langTitle)
+              langUl.append(langLi)
+            }
+            infoDiv.append(langUl)
+            console.log(languages)
+            //   Country flag
+            image = document.createElement('img')
+            image.src = country.flags.png
+            mainInfoDiv.append(image)
+
+            console.log(divMain)
+            // Final add
+            document.querySelector('.main-out').append(divMain)
+          }
+          else{
+            for(country of data){
+              item = document.createElement('h2')
+              item.textContent = country.name
+              document.querySelector('.main-out').append(item)
+  
+            }
+          }
         }
       })
       .catch(error => {
